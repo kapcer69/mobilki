591c3ed0117201468eaa7f6bd53800583546db96
Kacper Gotkowski <kgotkowski3@gmail.com>
2023-10-30 16:47:56 +0100

    Create Header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,13 @@
+<script>
+import "~/assets/css/Header.scss";
+</script>
+
 <template>
   <header class="header">
     <img src="~/assets/images/layout_set_logo.png" alt="Logo" />
-    <div class="account">
+    <button class="account">
       <p class="account-user">Admin</p>
       <Icon class="account-icon" name="material-symbols:account-circle" />
-    </div>
+    </button>
   </header>
 </template>
