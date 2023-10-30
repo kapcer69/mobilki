555f8aab70259d7c64a7be18d1fb93498d9f051f
Kacper Gotkowski <kgotkowski3@gmail.com>
2023-10-30 18:10:01 +0100

    Tweak global styles. Add a button to add an announcement

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <main>
     <Header />
-    <h1 class="title">Ogłoszenia</h1>
+    <div class="content-header">
+      <h1 class="title">Ogłoszenia</h1>
+      <button class="add" title="Dodaj ogłoszenie">
+        <Icon class="add-icon" name="material-symbols:add" />
+      </button>
+    </div>
     <Announcement class="color" />
     <Announcement class="color" />
     <Announcement class="color" />
